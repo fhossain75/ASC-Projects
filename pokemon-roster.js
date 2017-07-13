6bbdd74196dfded8dc6c45e09eec59dd54acd5ef
fhossainAC4 <faisal.hossain75@gmail.com>
2017-07-13 12:02:52 -0400

    array in array

---
+++ pokemon-roster.js
@@ -0,0 +1,8 @@
+var pokemonstats = [["NAME:", "TYPE:", "HP", "DEF", "ATK", "LEGEND"]
+                   ["NAME:", "TYPE:", "HP", "DEF", "ATK", "LEGEND"]
+                   ["NAME:", "TYPE:", "HP", "DEF", "ATK", "LEGEND"]
+                   ["NAME:", "TYPE:", "HP", "DEF", "ATK", "LEGEND"]
+                   ["NAME:", "TYPE:", "HP", "DEF", "ATK", "LEGEND"]
+                   ["NAME:", "TYPE:", "HP", "DEF", "ATK", "LEGEND"]]
+pokemonstats[0][0] = "NAME: Bulbasaur"
+console.log(pokemonstats)
